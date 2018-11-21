61477e3dae7aa372ab1d1b33aac9197f6b21691c
Kerawos <Maresowa@gmail.com>
2018-11-21 17:09:36 +0100

    add emplpoyy and company

--- src/pl/mareksowa/akademiakodu/Company.java
+++ src/pl/mareksowa/akademiakodu/Company.java
@@ -1,4 +1,58 @@
 package pl.mareksowa.akademiakodu;
 
 public class Company {
+
+    private String name;
+    private String headquaters;
+    private Employee[] employes;
+    private Asset[] assets;
+    private int money;
+
+    public Company(String name, String headquaters, int assetSize, int employeeSize, int money) {
+        this.name = name;
+        this.headquaters = headquaters;
+        this.employes = new Employee[employeeSize];
+        this.assets = new Asset[assetSize];
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeadquaters() {
+        return headquaters;
+    }
+
+    public void setHeadquaters(String headquaters) {
+        this.headquaters = headquaters;
+    }
+
+    public Employee[] getEmployes() {
+        return employes;
+    }
+
+    public void setEmployes(Employee[] employes) {
+        this.employes = employes;
+    }
+
+    public Asset[] getAssets() {
+        return assets;
+    }
+
+    public void setAssets(Asset[] assets) {
+        this.assets = assets;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
--- src/pl/mareksowa/akademiakodu/Employee.java
+++ src/pl/mareksowa/akademiakodu/Employee.java
@@ -1,4 +1,38 @@
 package pl.mareksowa.akademiakodu;
 
 public class Employee {
+
+    private String name;
+    private int experiance;
+    private int salary;
+
+    public Employee(String name, int experiance, int salary) {
+        this.name = name;
+        this.experiance = experiance;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getExperiance() {
+        return experiance;
+    }
+
+    public void setExperiance(int experiance) {
+        this.experiance = experiance;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
